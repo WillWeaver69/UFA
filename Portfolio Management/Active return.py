6f772df0f7bd6f3299872833533da477c1ccf634
WillWeaver69 <133113942+WillWeaver69@users.noreply.github.com>
2023-12-16 21:56:25 +0100

    Create Active return.py

---
+++ Portfolio Management/Active return.py
@@ -0,0 +1,6 @@
+def calculate_active_return(expected_return_activeportfolio, expected_return_benchmark):
+    return expected_return_activeportfolio - expected_return_benchmark
+
+
+#Example usage
+calculate_active_return(0.15, 0.12)
